0673d0508167f60f753078a30c7b336a4583585b
hsiang0100 <myluck0482@gmail.com>
2019-11-06 13:47:06 +0800

    建立對應專案

---
+++ ReivewCode/Position.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReivewCode
+{
+	public class Position
+	{
+		int? m_nRAddress;
+		int? m_nNo;
+
+		public int? No
+		{
+			get
+			{
+				return m_nNo;
+			}
+			set
+			{
+				m_nNo = value;
+			}
+		}
+
+	}
+}
+
+
+
